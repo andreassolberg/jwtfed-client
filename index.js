f2443f8841a14fecb42d2634ec45cdcadb6b6dcd
Andreas Åkre Solberg <andreas.solberg@uninett.no>
2018-08-10 12:30:05 +0200

    Addding config, webfinger resolve and more

--- index.js
+++ index.js
@@ -1,23 +1,54 @@
 
 const
   Client = require('./lib/Client'),
-  Router = require('koa-router')
+  Router = require('koa-router'),
+  morgan = require('koa-morgan'),
+  JWTWebFinger = require('./lib/JWTWebFinger'),
+  nconf = require('nconf'),
+  errors = require('koa-errors')
 
 const Koa = require('koa');
 const app = new Koa();
-const router = new Router();
 const PORT = 3000
 
-const client = new Client('https://jwt-client.andreas.labs.uninett.no/yay')
+nconf.argv()
+  .env({
+    "separator": '__',
+    "lowerCase": true
+  })
+  .file({ file: 'etc/config.json' })
 
-router.get('/', async (ctx, next) => {
+console.log("ISS", nconf.get('iss'))
+
+nconf.required(['iss']);
+
+const healthcheck = new Router();
+healthcheck.get('/', async (ctx, next) => {
   ctx.body = 'OK!'
 })
 
+
+const webfinger = new JWTWebFinger(nconf.get('iss'), nconf.get('metadata'), nconf.get('authorityHints'), nconf.get('kid'), nconf.get('jwks'))
+
+
+
+const client = new Client(nconf.get('iss'))
+
+const router = new Router();
+router.get('/', async (ctx, next) => {
+  ctx.body = 'OK!'
+})
 router.get('/auth', async (ctx, next) => {
-  let url = await client.getAuthenticationRequest()
-  console.log("Sending user ahead with this authentication request " + url)
-  ctx.redirect(url)
+
+  if (ctx.query && ctx.query.id) {
+    let url = await client.getAuthenticationRequest(ctx.query.id)
+    console.log("Sending user ahead with this authentication request " + url)
+    ctx.redirect(url)
+  } else {
+    throw new Error("Missing query parameter id. Use the provider ID of the provider you would like to authenticate with.")
+  }
+
+
 })
 router.get('/callback', async (ctx, next) => {
   ctx.body = 'Hello callback()'
@@ -25,6 +56,10 @@ router.get('/callback', async (ctx, next) => {
 
 
 app
+  .use(errors())
+  .use(morgan('combined'))
+  .use(healthcheck.routes())
+  .use(webfinger.routes())
   .use(router.routes())
   .use(router.allowedMethods());
 app.listen(PORT)
